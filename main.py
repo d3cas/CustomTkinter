9473aa0630705b3c8a5ccdeb9d4fcbdc604931ec
decas carlos <decascarlos57@gmail.com>
2024-12-16 12:39:58 +0100

    hello world, customtkinter

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ class main (ctk.CTk) :
   def __init__(self) :
     super().__init__()
 
-    self.geometry ("400x400") # definindo as dimensões da janela
-    self.title ("window")     # titulo da janela
+    self.geometry ("400x400") # set window dimension
+    self.title ("window")     # window title
 
-    # criando um label
+    # create a label
     self.lbl = ctk.CTkLabel (
         master="self",
         text="hello, world"
